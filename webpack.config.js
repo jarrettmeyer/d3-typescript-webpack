f7e68a3be2a15ce6a3ab5cb194fe44f4bc76a18a
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-07-12 12:50:00 -0400

    Adds progress plugin to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 
 module.exports = {
@@ -25,6 +26,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html"
-        })
+        }),
+        new webpack.ProgressPlugin()
     ]
 };
